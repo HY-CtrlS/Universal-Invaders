8e070dbed588808d6554b7656a0d6c225dd7c7d7
Geguuuul <tjdud020512s@naver.com>
2024-11-19 20:56:55 +0900

    [CTRLS-37] 공격력 증가, 탄속 증가 아이템 추가
    스텟증가 아이템과 구조는 똑같습니다. 현재는 구체적인 수치를 정하기 보다는 단순 1로 설정해 두었습니다. 이후에 플레이 해보면서 밸런스를 잡아야 할 것 같습니다.

---
+++ src/Item/AttackPowerUpItem.java
@@ -0,0 +1,106 @@
+package Item;
+
+import engine.Core;
+import engine.DrawManager.SpriteType;
+import engine.StatusManager;
+import java.awt.Color;
+import java.util.logging.Logger;
+
+public class AttackPowerUpItem implements Item {
+
+    // 이속증가 아이템의 최대 레벨
+    private static final int MAX_LEVEL = 5;
+    // 이속증가 아이템의 현재 레벨
+    private int level = 0;
+    // 이속증가 아이템의 스프라이트 타입
+    private SpriteType spriteType = SpriteType.AttackSpeedUpItem;
+    // 현재 아이템을 보유중인지에 대한 변수
+    private boolean isowned = false;
+    // Status 싱글톤 객체를 위한 변수
+    private StatusManager status;
+    // Logger
+    private Logger logger;
+    // 아이템의 색깔
+    private Color color;
+
+    // 공속증가 아이템 객체 생성자
+    public AttackPowerUpItem(final int level) {
+        logger = Core.getLogger();
+        this.level = level;
+        status = Core.getStatusManager();
+        this.color = Color.RED;
+    }
+
+    @Override
+    public String getItemDescription() {
+        return "Upgrade your Weapon";
+    }
+
+    @Override
+    public String getItemEffectDescription() {
+        return "Attack Power : + 1";
+    }
+
+    @Override
+    public String getItemName() {
+        return "Heavy Missile";
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public void setOwned(final boolean owned) {
+        this.isowned = owned;
+    }
+
+    @Override
+    public boolean isOwned() {
+        return this.isowned;
+    }
+
+    @Override
+    public SpriteType getSpriteType() {
+        return this.spriteType;
+    }
+
+    @Override
+    public void setSpriteType(SpriteType spriteType) {
+        this.spriteType = spriteType;
+    }
+
+    @Override
+    public int activateItem() {
+        if (level < MAX_LEVEL) {
+            status.setBaseDamage(status.getBaseDamage() + 1);
+            this.logger.info("Attack Power Up!! + 1");
+            increaseLevel();
+            this.logger.info("Attack Power Level : " + getLevel());
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isMaxLevel() {
+        return (this.level == MAX_LEVEL);
+    }
+
+    @Override
+    public void increaseLevel() {
+        if (this.level < MAX_LEVEL) {
+            this.level++;
+        }
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return MAX_LEVEL;
+    }
+}
---
+++ src/Item/BulletSpeedUpItem.java
@@ -0,0 +1,106 @@
+package Item;
+
+import engine.Core;
+import engine.DrawManager.SpriteType;
+import engine.StatusManager;
+import java.awt.Color;
+import java.util.logging.Logger;
+
+public class BulletSpeedUpItem implements Item {
+
+    // 이속증가 아이템의 최대 레벨
+    private static final int MAX_LEVEL = 5;
+    // 이속증가 아이템의 현재 레벨
+    private int level = 0;
+    // 이속증가 아이템의 스프라이트 타입
+    private SpriteType spriteType = SpriteType.AttackSpeedUpItem;
+    // 현재 아이템을 보유중인지에 대한 변수
+    private boolean isowned = false;
+    // Status 싱글톤 객체를 위한 변수
+    private StatusManager status;
+    // Logger
+    private Logger logger;
+    // 아이템의 색깔
+    private Color color;
+
+    // 공속증가 아이템 객체 생성자
+    public BulletSpeedUpItem(final int level) {
+        logger = Core.getLogger();
+        this.level = level;
+        status = Core.getStatusManager();
+        this.color = Color.GRAY;
+    }
+
+    @Override
+    public String getItemDescription() {
+        return "Upgrade your Bullet";
+    }
+
+    @Override
+    public String getItemEffectDescription() {
+        return "Bullet Speed : + 1";
+    }
+
+    @Override
+    public String getItemName() {
+        return "Silver Bullet";
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public void setOwned(final boolean owned) {
+        this.isowned = owned;
+    }
+
+    @Override
+    public boolean isOwned() {
+        return this.isowned;
+    }
+
+    @Override
+    public SpriteType getSpriteType() {
+        return this.spriteType;
+    }
+
+    @Override
+    public void setSpriteType(SpriteType spriteType) {
+        this.spriteType = spriteType;
+    }
+
+    @Override
+    public int activateItem() {
+        if (level < MAX_LEVEL) {
+            status.setBulletSpeed(status.getBulletSpeed() + 1);
+            this.logger.info("BulletSpeed Up!! + 1");
+            increaseLevel();
+            this.logger.info("BulletSpeed Level : " + getLevel());
+        }
+        return 3;
+    }
+
+    @Override
+    public boolean isMaxLevel() {
+        return (this.level == MAX_LEVEL);
+    }
+
+    @Override
+    public void increaseLevel() {
+        if (this.level < MAX_LEVEL) {
+            this.level++;
+        }
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return MAX_LEVEL;
+    }
+}
