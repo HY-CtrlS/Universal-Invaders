de4314c608ea104ff3953f5bf720d0b23e66174f
Koominjae <koom0406@naver.com>
2024-12-01 04:37:29 +0900

    [CTRLS-78] feature: 크리스탈 이동 메소드 추가
    최종 페이즈4 발악 패턴에서 크리스탈이 정해진 루트를 따라 이동하는 update() 메소드 추가.
    
    	modified:   src/main/java/kr/ac/hanyang/entity/boss/Crystal.java

--- src/main/java/kr/ac/hanyang/entity/boss/Crystal.java
+++ src/main/java/kr/ac/hanyang/entity/boss/Crystal.java
@@ -7,6 +7,7 @@ import kr.ac.hanyang.entity.Entity;
 public class Crystal extends Entity {
 
     private int hp;
+    private int speed;
     private boolean isBroken;
 
     public Crystal(final int positionX, final int positionY) {
@@ -17,7 +18,41 @@ public class Crystal extends Entity {
         this.spriteType = SpriteType.Crystal;
 
         this.hp = 50;
+        this.speed = 2;
         this.isBroken = false;
+    }
+
+    public void update() {
+        final int minX = 30;
+        final int maxX = 650;
+        final int minY = 200;
+        final int maxY = 680;
+
+        boolean up = this.positionY == minY;
+        boolean down = this.positionY == maxY;
+        boolean right = this.positionX == maxX;
+        boolean left = this.positionX == minX;
+
+        if (up && left) {
+            this.positionY += speed;
+        } else if (down && left) {
+            this.positionX += speed;
+        } else if (down && right) {
+            this.positionY -= speed;
+        } else if (up && right) {
+            this.positionX -= speed;
+        } else if (up) {
+            this.positionX -= speed;
+        } else if (left) {
+            this.positionY += speed;
+        } else if (down) {
+            this.positionX += speed;
+        } else if (right) {
+            this.positionY -= speed;
+        }
+
+        this.positionX = Math.max(minX, Math.min(this.positionX, maxX));
+        this.positionY = Math.max(minY, Math.min(this.positionY, maxY));
     }
 
     public final boolean isBroken() {
