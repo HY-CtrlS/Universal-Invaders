29a0cc78ece7dba852579ed966c82389a5760af5
Koominjae <koom0406@naver.com>
2024-12-05 17:32:39 +0900

    fix: 테스트용 코드 수정 원상복구

--- src/main/java/kr/ac/hanyang/screen/GameScreen.java
+++ src/main/java/kr/ac/hanyang/screen/GameScreen.java
@@ -36,7 +36,7 @@ public class GameScreen extends Screen {
     /** 기본 적 생성 간격 */
     private static final int ENEMY_SPAWN_INTERVAL = 2000;
     // 레벨 클리어 조건 시간
-    private static final int LEVEL_CLEAR_TIME = 3;
+    private static final int LEVEL_CLEAR_TIME = 300;
 
     /** Formation of enemy ships. */
     private EnemyShipSet enemyShipSet;
